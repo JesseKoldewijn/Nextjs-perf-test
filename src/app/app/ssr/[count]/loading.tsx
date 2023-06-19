78ea0c66ede24cde59a907165ceb670a0a545411
JesseKoldewijn <jesse.koldewijn@denieuwezaak.nl>
2023-06-19 09:36:17 +0200

    feat: loading.tsx files for app router paths

---
+++ src/app/app/ssr/[count]/loading.tsx
@@ -0,0 +1,12 @@
+import Loading from "@/components/Loading";
+import React from "react";
+
+const LoadingPlaceholder = () => {
+  return (
+    <div>
+      <h1>SSR - Pages</h1>
+      <Loading />
+    </div>
+  );
+};
+export default LoadingPlaceholder;
